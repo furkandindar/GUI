6ebfa3fbe2b062339f03417834f9c72b7ed6ad2f
furkandindar <fdindar1@binghamton.edu>
2018-01-07 23:37:00 -0500

    first commit

---
+++ EasyPass/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace iWallet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnParola1 = new System.Windows.Forms.Button();
+            this.btnParola2 = new System.Windows.Forms.Button();
+            this.btnParola3 = new System.Windows.Forms.Button();
+            this.btnParola4 = new System.Windows.Forms.Button();
+            this.btnParola5 = new System.Windows.Forms.Button();
+            this.btnParola6 = new System.Windows.Forms.Button();
+            this.btnParola7 = new System.Windows.Forms.Button();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.btnParola8 = new System.Windows.Forms.Button();
+            this.btnParola9 = new System.Windows.Forms.Button();
+            this.btnParola10 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnParola1
+            // 
+            this.btnParola1.Location = new System.Drawing.Point(8, 91);
+            this.btnParola1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola1.Name = "btnParola1";
+            this.btnParola1.Size = new System.Drawing.Size(153, 28);
+            this.btnParola1.TabIndex = 0;
+            this.btnParola1.Text = "Password 1";
+            this.btnParola1.UseVisualStyleBackColor = true;
+            this.btnParola1.Click += new System.EventHandler(this.btnParola1_Click);
+            // 
+            // btnParola2
+            // 
+            this.btnParola2.Location = new System.Drawing.Point(8, 127);
+            this.btnParola2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola2.Name = "btnParola2";
+            this.btnParola2.Size = new System.Drawing.Size(153, 28);
+            this.btnParola2.TabIndex = 1;
+            this.btnParola2.Text = "Password 2";
+            this.btnParola2.UseVisualStyleBackColor = true;
+            this.btnParola2.Click += new System.EventHandler(this.btnParola2_Click);
+            // 
+            // btnParola3
+            // 
+            this.btnParola3.Location = new System.Drawing.Point(8, 162);
+            this.btnParola3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola3.Name = "btnParola3";
+            this.btnParola3.Size = new System.Drawing.Size(153, 28);
+            this.btnParola3.TabIndex = 2;
+            this.btnParola3.Text = "Password 3";
+            this.btnParola3.UseVisualStyleBackColor = true;
+            this.btnParola3.Click += new System.EventHandler(this.btnParola3_Click);
+            // 
+            // btnParola4
+            // 
+            this.btnParola4.Location = new System.Drawing.Point(8, 198);
+            this.btnParola4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola4.Name = "btnParola4";
+            this.btnParola4.Size = new System.Drawing.Size(153, 28);
+            this.btnParola4.TabIndex = 4;
+            this.btnParola4.Text = "Password 4";
+            this.btnParola4.UseVisualStyleBackColor = true;
+            this.btnParola4.Click += new System.EventHandler(this.btnParola4_Click);
+            // 
+            // btnParola5
+            // 
+            this.btnParola5.Location = new System.Drawing.Point(8, 234);
+            this.btnParola5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola5.Name = "btnParola5";
+            this.btnParola5.Size = new System.Drawing.Size(153, 28);
+            this.btnParola5.TabIndex = 5;
+            this.btnParola5.Text = "Password 5";
+            this.btnParola5.UseVisualStyleBackColor = true;
+            this.btnParola5.Click += new System.EventHandler(this.btnParola5_Click);
+            // 
+            // btnParola6
+            // 
+            this.btnParola6.Location = new System.Drawing.Point(8, 270);
+            this.btnParola6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola6.Name = "btnParola6";
+            this.btnParola6.Size = new System.Drawing.Size(153, 28);
+            this.btnParola6.TabIndex = 6;
+            this.btnParola6.Text = "Password 6";
+            this.btnParola6.UseVisualStyleBackColor = true;
+            this.btnParola6.Click += new System.EventHandler(this.btnParola6_Click);
+            // 
+            // btnParola7
+            // 
+            this.btnParola7.Location = new System.Drawing.Point(8, 305);
+            this.btnParola7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola7.Name = "btnParola7";
+            this.btnParola7.Size = new System.Drawing.Size(153, 28);
+            this.btnParola7.TabIndex = 7;
+            this.btnParola7.Text = "Password 7";
+            this.btnParola7.UseVisualStyleBackColor = true;
+            this.btnParola7.Click += new System.EventHandler(this.btnParola7_Click);
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(0, 50);
+            this.tbUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbUserName.Multiline = true;
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(169, 31);
+            this.tbUserName.TabIndex = 8;
+            // 
+            // btnParola8
+            // 
+            this.btnParola8.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnParola8.Location = new System.Drawing.Point(8, 341);
+            this.btnParola8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola8.Name = "btnParola8";
+            this.btnParola8.Size = new System.Drawing.Size(153, 28);
+            this.btnParola8.TabIndex = 9;
+            this.btnParola8.Text = "Password 8";
+            this.btnParola8.UseVisualStyleBackColor = true;
+            this.btnParola8.Click += new System.EventHandler(this.btnParola8_Click);
+            // 
+            // btnParola9
+            // 
+            this.btnParola9.Location = new System.Drawing.Point(8, 377);
+            this.btnParola9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola9.Name = "btnParola9";
+            this.btnParola9.Size = new System.Drawing.Size(153, 28);
+            this.btnParola9.TabIndex = 10;
+            this.btnParola9.Text = "Password 9";
+            this.btnParola9.UseVisualStyleBackColor = true;
+            this.btnParola9.Click += new System.EventHandler(this.btnParola9_Click);
+            // 
+            // btnParola10
+            // 
+            this.btnParola10.Location = new System.Drawing.Point(8, 414);
+            this.btnParola10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnParola10.Name = "btnParola10";
+            this.btnParola10.Size = new System.Drawing.Size(153, 28);
+            this.btnParola10.TabIndex = 11;
+            this.btnParola10.Text = "Password 10";
+            this.btnParola10.UseVisualStyleBackColor = true;
+            this.btnParola10.Click += new System.EventHandler(this.btnParola10_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.btnGoster);
+            this.panel1.Controls.Add(this.tbUserName);
+            this.panel1.Controls.Add(this.btnParola10);
+            this.panel1.Controls.Add(this.btnParola2);
+            this.panel1.Controls.Add(this.btnParola9);
+            this.panel1.Controls.Add(this.btnParola1);
+            this.panel1.Controls.Add(this.btnParola8);
+            this.panel1.Controls.Add(this.btnParola3);
+            this.panel1.Controls.Add(this.btnParola7);
+            this.panel1.Controls.Add(this.btnParola4);
+            this.panel1.Controls.Add(this.btnParola6);
+            this.panel1.Controls.Add(this.btnParola5);
+            this.panel1.Location = new System.Drawing.Point(11, 5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(172, 796);
+            this.panel1.TabIndex = 12;
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.AllowDrop = true;
+            this.btnGoster.BackColor = System.Drawing.Color.Transparent;
+            this.btnGoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGoster.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnGoster.Location = new System.Drawing.Point(0, 0);
+            this.btnGoster.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(171, 48);
+            this.btnGoster.TabIndex = 12;
+            this.btnGoster.Text = "▲ Show ▲";
+            this.btnGoster.UseVisualStyleBackColor = false;
+            this.btnGoster.MouseHover += new System.EventHandler(this.btnGoster_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 816);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "iWallet";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParola1;
+        private System.Windows.Forms.Button btnParola2;
+        private System.Windows.Forms.Button btnParola3;
+        private System.Windows.Forms.Button btnParola4;
+        private System.Windows.Forms.Button btnParola5;
+        private System.Windows.Forms.Button btnParola6;
+        private System.Windows.Forms.Button btnParola7;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Button btnParola8;
+        private System.Windows.Forms.Button btnParola9;
+        private System.Windows.Forms.Button btnParola10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGoster;
+    }
+}
+
---
+++ EasyPass/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iWallet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var designSize = this.ClientSize;
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.Size = designSize;
+            this.Text = "EasyPass";
+
+            this.DesktopLocation = new Point((Screen.PrimaryScreen.Bounds.Width - 130), 0);
+            this.Height = Screen.PrimaryScreen.Bounds.Height;
+
+           // tbUserName.Top = tbUserName.Top + 50;
+        }
+
+        int AllButtonHeight;
+        int FormHeight = 84;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Height = FormHeight;
+            //this.panel1.Height = 0;
+            //this.panel1.Width = 0;
+            panel1.Height = this.Height;
+            //panel1.Width = this.Width-20;
+
+            int ExtraScreen = 0;
+            if (System.Windows.Forms.Screen.AllScreens.Length > 1)
+            {
+                ExtraScreen = System.Windows.Forms.Screen.AllScreens[1].WorkingArea.X;
+            }
+
+            this.Left = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width - this.Width + ExtraScreen;
+            this.Top = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height - this.Height;
+            //this.FormBorderStyle = FormBorderStyle.None;
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+
+            AllButtonHeight = 0;
+            for (int i = 0; i < panel1.Controls.Count; i++)
+            {
+                AllButtonHeight = (panel1.Controls[i].Height + AllButtonHeight + 10);
+            }
+            AllButtonHeight += 40;
+        }
+
+        private void btnParola1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Parola1");
+            ClickFinish();
+        }
+
+        private void btnParola2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Parola2");
+            ClickFinish();
+        }
+
+        private void btnParola3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Parola3");
+            ClickFinish();
+        }
+
+        private void btnParola4_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Parola4");
+            ClickFinish();
+        }
+
+        private void btnParola5_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Parola5");
+            ClickFinish();
+        }
+
+        private void btnParola6_Click(object sender, EventArgs e)
+        {
+             Clipboard.SetText("Parola6");
+            ClickFinish();
+        }
+
+        private void btnParola7_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Parola7");
+            ClickFinish();
+        }
+
+        private void btnParola8_Click(object sender, EventArgs e)
+        {
+            tbUserName.Text = "Parola8";
+            Clipboard.SetText("UserName8");
+            ClickFinish();
+        }
+
+        private void btnParola9_Click(object sender, EventArgs e)
+        {
+            tbUserName.Text = "UserName9";
+            Clipboard.SetText("Parola9");
+            ClickFinish();
+        }
+
+        private void btnParola10_Click(object sender, EventArgs e)
+        {
+            tbUserName.Text = "UserName10";
+            Clipboard.SetText("Parola10");
+            ClickFinish();
+        }
+        
+        private void btnGoster_MouseHover(object sender, EventArgs e)
+        {
+            //this.Opacity = 100;
+            openMenu();
+        }
+        private void openMenu()
+        {
+            for (int k = FormHeight; k < AllButtonHeight; k = k + 10)
+            {
+                this.Height = k;
+                panel1.Height = this.Height;
+                
+
+                this.Top = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height - this.Height;
+
+            }
+            btnGoster.Visible = false;
+            //panel1.Width = this.Width - 30;
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            closeMenu();
+            //this.Opacity = 30;
+        }
+
+        private void closeMenu()
+        {
+
+            this.Height = FormHeight;
+            this.Top = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height - this.Height;
+            btnGoster.Visible = true;
+        }
+
+        private void ClickFinish()
+        {
+            closeMenu();
+            //Opacity = 30;
+        }
+
+
+    }
+}
