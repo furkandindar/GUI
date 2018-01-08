9fde66a5a612e6355271ce908b93a701c9a454a4
furkandindar <fdindar1@binghamton.edu>
2018-01-07 23:18:22 -0500

    first commit

---
+++ WF BouncingBall/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StopButton = New System.Windows.Forms.Button()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'StopButton
+        '
+        Me.StopButton.Location = New System.Drawing.Point(101, 0)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(75, 23)
+        Me.StopButton.TabIndex = 0
+        Me.StopButton.Text = "Stop"
+        Me.StopButton.UseVisualStyleBackColor = True
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(-1, 0)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(75, 23)
+        Me.StartButton.TabIndex = 1
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(212, 0)
+        Me.TrackBar1.Maximum = 200
+        Me.TrackBar1.Minimum = 50
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.RightToLeftLayout = True
+        Me.TrackBar1.Size = New System.Drawing.Size(469, 56)
+        Me.TrackBar1.TabIndex = 2
+        Me.TrackBar1.Value = 125
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(696, 0)
+        Me.TrackBar2.Maximum = 1
+        Me.TrackBar2.Minimum = -1
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Size = New System.Drawing.Size(218, 56)
+        Me.TrackBar2.TabIndex = 3
+        '
+        'TrackBar3
+        '
+        Me.TrackBar3.Location = New System.Drawing.Point(944, 0)
+        Me.TrackBar3.Maximum = 1
+        Me.TrackBar3.Minimum = -1
+        Me.TrackBar3.Name = "TrackBar3"
+        Me.TrackBar3.Size = New System.Drawing.Size(212, 56)
+        Me.TrackBar3.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(780, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "x value"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(1023, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "y value"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(437, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "speed"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1223, 513)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrackBar3)
+        Me.Controls.Add(Me.TrackBar2)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.StopButton)
+        Me.Controls.Add(Me.StartButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents StopButton As Button
+    Friend WithEvents StartButton As Button
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents TrackBar2 As TrackBar
+    Friend WithEvents TrackBar3 As TrackBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ WF BouncingBall/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+
+    Dim radius As Integer = 20
+    Dim velocity As Integer = 50
+    Dim xC As Integer, yC As Integer, xDelta As Integer, yDelta As Integer, xSize As Integer, ySize As Integer
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        Timer1.Start()
+    End Sub
+
+    Public Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        velocity = TrackBar1.Value
+    End Sub
+
+    Private Sub TrackBar2_Scroll(sender As Object, e As EventArgs) Handles TrackBar2.Scroll
+        xDelta = TrackBar2.Value
+    End Sub
+
+    Private Sub TrackBar3_Scroll(sender As Object, e As EventArgs) Handles TrackBar3.Scroll
+        yDelta = TrackBar3.Value
+    End Sub
+
+    Private Sub Form1(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1_Tick_1(sender, e)
+    End Sub
+
+    Private Sub Timer1_Tick_1(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Start()
+        Timer1.Interval = velocity
+        DrawBall()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As System.EventArgs) Handles MyBase.Resize
+        xSize = Me.ClientSize.Width
+        ySize = Me.ClientSize.Height
+        xC = xSize / 2
+        yC = ySize / 2
+        Me.Invalidate()
+        DrawBall()
+    End Sub
+
+    Private Sub DrawBall()
+        Dim g As Graphics = Me.CreateGraphics()
+        Dim b As Brush = New SolidBrush(Me.BackColor)
+        g.FillEllipse(b, xC - radius, yC - radius, 2 * radius, 2 * radius)
+        xC = xC + (xDelta * 90)
+        yC = yC + (yDelta * 90)
+
+        If (xC + radius >= ClientSize.Width) OrElse (xC - radius <= 0) Then
+            xDelta = -xDelta
+        End If
+        If (yC + radius >= ClientSize.Height) OrElse (yC - radius <= 0) Then
+            yDelta = -yDelta
+        End If
+
+        b = New SolidBrush(Color.DeepSkyBlue)
+        g.FillEllipse(b, xC - radius, yC - radius, 2 * radius, 2 * radius)
+        b.Dispose()
+        g.Dispose()
+    End Sub
+
+End Class
